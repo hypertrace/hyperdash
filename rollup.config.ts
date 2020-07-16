9748b5350e8274d72971b5053bb12a8858971f98
Aaron Steinfeld <aaron@traceable.ai>
2020-07-16 14:54:54 -0400

    fix: restoring umd format

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,4 +1,5 @@
 // tslint:disable
+import { camelCase } from 'lodash';
 import commonjs from 'rollup-plugin-commonjs';
 import json from '@rollup/plugin-json';
 import resolve from '@rollup/plugin-node-resolve';
@@ -11,7 +12,20 @@ const libraryName = 'hyperdash';
 
 export default {
   input: `src/${libraryName}.ts`,
-  output: [{ file: pkg.module, format: 'es', sourcemap: true }],
+  output: [
+    {
+      file: pkg.main,
+      name: camelCase(libraryName),
+      format: 'umd',
+      sourcemap: true,
+      globals: {
+        'lodash-es': '_',
+        rxjs: 'rxjs',
+        'rxjs/operators': 'rxjs.operators'
+      }
+    },
+    { file: pkg.module, format: 'es', sourcemap: true }
+  ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
   external: ['lodash-es', 'rxjs', 'rxjs/operators', 'core-js/es7/reflect'],
   watch: {
